5fecc4afb4b999df9d2b5d6a1c9e8932d45dac0a
enzorusso <enzorussor1@gmail.com>
2023-08-02 09:24:12 -0300

    Alterando estilizaçao da tela de lembretes

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -5,16 +5,22 @@ import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 
 
 function App() {
+  const addToDo = () => {
+    console.log('chegou');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <div className='Content'>
         <p>LEMBRETES</p>
         <div className='List'>
           <p className='None'> Nenhum lembrete disponível. </p>
-          <button className='Add-button'>
+          <button className='Add-button' onClick={addToDo}>
             <FontAwesomeIcon icon={faCirclePlus}></FontAwesomeIcon>&nbsp;
               Novo Lembrete
           </button>
+        </div>
         </div>
       </header>
     </div>
